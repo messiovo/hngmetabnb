097634abf43d28b6a14f2599d124927797f54175
Messiovo <messiovo@gmail.com>
2022-11-13 21:44:19 +0100

    added features

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Logo } from '../asset/icons';
 
+const navLinks = [
+    {
+      text: 'Home',
+      to: '/',
+    },
+    {
+      text: 'Place to stay',
+      to: '/place-to-stay',
+    },
+    {
+      text: 'NFTs',
+      to: '/nfts',
+    },
+    {
+      text: 'Community',
+      to: '/community',
+    },
+  ];
 const Navbar = () => {
+
   return (
-    <div>Navbar</div>
+    <div>
+        <Logo/>
+    </div>
   )
 }
 
