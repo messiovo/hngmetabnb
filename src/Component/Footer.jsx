2a7087dbc06b0810f8bf80949cd0e6708c0d744b
Messiovo <messiovo@gmail.com>
2022-11-14 18:10:45 +0100

    creating icons

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FooterLogo, Facebook, Instagram, Twitter } from '../asset/icons';
 
+const links = [
+  {
+    heading: 'Community',
+    links: [
+      {
+        text: 'NFT',
+        to: '/nfts',
+      },
+      {
+        text: 'Tokens',
+        to: '/tokens',
+      },
+      {
+        text: 'Landlords',
+        to: '/landlords',
+      },
+      {
+        text: 'Discord',
+        to: '/discord',
+      },
+    ],
+  },
+  {
+    heading: 'Places',
+    links: [
+      {
+        text: 'Castle',
+        to: '/castle',
+      },
+      {
+        text: 'Farms',
+        to: '/farms',
+      },
+      {
+        text: 'Villa',
+        to: '/beach',
+      },
+      {
+        text: 'Learn more',
+        to: '/learn-more',
+      },
+    ],
+  },
+  {
+    heading: 'About us',
+    links: [
+      {
+        text: 'Road map',
+        to: '/road-map',
+      },
+      {
+        text: 'Creators',
+        to: '/creators',
+      },
+      {
+        text: 'Career',
+        to: '/career',
+      },
+      {
+        text: 'Contact us',
+        to: '/contact-us',
+      },
+    ],
+  },
+];
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className={style.Footer}>
+      <div className={style.Footer__left}>
+        <FooterLogo />
+        <div className={style.Footer__left__socials}>
+          <Facebook />
+          <Instagram />
+          <Twitter />
+        </div>
+        <p>&copy; 2022 Metabnb</p>
+      </div>
+      <div>
+        {links.map(({ heading, links: link }) => (
+          <div key={heading}>
+            <h3>{heading}</h3>
+            <div>
+              {link.map(({ text, to }) => (
+                <Link to={to} key={text}>
+                  <small>{text}</small>
+                </Link>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Footer
--- src/asset/icons/index.js
+++ src/asset/icons/index.js
@@ -1,4 +1,5 @@
 export { default as Logo } from "./Logo";
 export { default as Like } from "./Like";
 export { default as Star } from "./Star";
+export { default as FooterLogo } from "./FooterLogo";
 export { default as Links } from "../../Component/atoms/Links";
