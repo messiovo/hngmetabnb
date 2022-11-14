9a5786096391a78d0e9c2a9d7c9f4780bb677d9a
Messiovo <messiovo@gmail.com>
2022-11-14 19:42:22 +0100

    update on styling

--- src/Pages/PlaceToStay.jsx
+++ src/Pages/PlaceToStay.jsx
@@ -1,104 +1,169 @@
-import React from 'react';
-import { PlaceCard } from '../Component/atoms';
+import React from "react";
+import { PlaceCard } from "../Component/atoms";
 
-
-const filters = [
-    'Restuarant',
-    'Cottage',
-    'Castle',
-    'fantast city',
-    'beach',
-    'Carbins',
-    'Off-grid',
-    'Farm',
-  ];
+const contents = [
+  "Restuarant",
+  "Cottage",
+  "Castle",
+  "fantast city",
+  "beach",
+  "Carbins",
+  "Off-grid",
+  "Farm",
+];
 
 const placeDetails = [
-    {
-        image:  require("../asset/images/places1.png"),
-        title: 'Desert king',
-        payment: '1MBT per night',
-        distance: '2345km away',
-        availability: 'available for 2weeks stay',
-        stars: 5,
-      },
-      {
-        image:  require("../asset/images/places2.png"),
-        title: 'Desert king',
-        payment: '1MBT per night',
-        distance: '2345km away',
-        availability: 'available for 2weeks stay',
-        stars: 5,
-      },
-      {
-        image:  require("../asset/images/places3.png"),
-        title: 'Desert king',
-        payment: '1MBT per night',
-        distance: '2345km away',
-        availability: 'available for 2weeks stay',
-        stars: 5,
-      },
-      {
-        image:  require("../asset/images/places4.png"),
-        title: 'Desert king',
-        payment: '1MBT per night',
-        distance: '2345km away',
-        availability: 'available for 2weeks stay',
-        stars: 5,
-      },
-      {
-        image:  require("../asset/images/places5.png"),
-        title: 'Desert king',
-        payment: '1MBT per night',
-        distance: '2345km away',
-        availability: 'available for 2weeks stay',
-        stars: 5,
-      },
-      {
-        image:  require("../asset/images/places6.png"),
-        title: 'Desert king',
-        payment: '1MBT per night',
-        distance: '2345km away',
-        availability: 'available for 2weeks stay',
-        stars: 5,
-      },
-      {
-        image:  require("../asset/images/places7.png"),
-        title: 'Desert king',
-        payment: '1MBT per night',
-        distance: '2345km away',
-        availability: 'available for 2weeks stay',
-        stars: 5,
-      },
-      {
-        image:  require("../asset/images/places8.png"),
-        title: 'Desert king',
-        payment: '1MBT per night',
-        distance: '2345km away',
-        availability: 'available for 2weeks stay',
-        stars: 5,
-      },
-  ];
+  {
+    image: require("../asset/images/places5.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places6.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places7.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places8.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places1.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places2.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places3.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places4.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places9.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places10.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places11.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places12.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places13.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places14.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places15.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  },
+  {
+    image: require("../asset/images/places16.png"),
+    title: "Desert king",
+    payment: "1MBT per night",
+    distance: "2345km away",
+    availability: "available for 2weeks stay",
+    stars: 5,
+  }
+];
 
 const PlaceToStay = () => {
   return (
-    <section>
+    <section className="py-10 lg:px-12 px-4">
       <div>
-        {filters.map((filter) => (
-          <button key={filter} type="button">{filter}</button>
+        {contents.map((items) => (
+          <button key={items} type="button">
+            {items}
+          </button>
         ))}
-        <div >
+        <div>
           <span>Location</span>
-          {/* <Filter /> */}
+          <Location />
         </div>
       </div>
-      <div>
-      {placeDetails.map((place) => (
-        <PlaceCard key={place.image} relativeUrl="place-to-stay" {...place} />
-      ))}
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6">
+        {placeDetails.map((place) => (
+          <PlaceCard key={place.image} relativeUrl="place-to-stay" {...place} />
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default PlaceToStay
+export default PlaceToStay;
