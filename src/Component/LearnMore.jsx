72b2ea2d63c29e87387d2f3052d950c6e13571b1
Messiovo <messiovo@gmail.com>
2022-11-14 17:01:17 +0100

    import images

--- src/Component/LearnMore.jsx
+++ src/Component/LearnMore.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const LearnMore = () => {
   return (
-    <div>LearnMore</div>
+    <section >
+      <div>
+        <h2>Metabnb NFTs</h2>
+        <p>
+          Discover our NFT gift cards collection. Loyal customers gets amazing
+          gift cards which are traded as NFTs. These NFTs gives our cutomer
+          access to loads of our exclusive services.
+        </p>
+        <button type="button">
+          <span>Learn more</span>
+        </button>
+      </div>
+      <div>
+        <img src={}  />
+        <img src={}  />
+        <img src={}  />
+      </div>
+      </section>
   )
 }
 
