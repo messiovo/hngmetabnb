1f42c80ba91b08df7c63d3cfd54ae5fa0ac8b452
Messiovo <messiovo@gmail.com>
2022-11-15 15:34:45 +0100

    fixes

--- src/Pages/PlaceToStay.jsx
+++ src/Pages/PlaceToStay.jsx
@@ -146,17 +146,17 @@ const placeDetails = [
 
 const PlaceToStay = () => {
   return (
-    <section className="py-10 lg:px-12 px-4 mt-20">
-      <div className="grid lg:grid-cols-9 grid-cols-3 gap-4 py-10">
+    <section className="py-10 lg:px-12 px-4 lg:mt-20 mt-5">
+      <div className="py-10 flex flex-wrap justify-between lg:gap-0 gap-4">
         {contents.map((items) => (
           <button key={items} type="button">
             {items}
           </button>
         ))}
-        <div className="flex gap-2 border border-black/[0.1] py-2 px-4 items-center mx-auto">
+        <button className="flex gap-2 border border-black/[0.1] p-2 items-center cursor-pointer self-center rounded-lg ">
           <span>Location</span>
           <Locale />
-        </div>
+        </button>
       </div>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6">
         {placeDetails.map((place) => (
